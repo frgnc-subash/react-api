1b4de0d7b2741b3aba1272197c34b67a8aeb3657
frgnc-subash <axosis.social357@gmail.com>
2025-07-22 08:40:36 +0545

    payload updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,22 @@ import { useRoutes } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 import UserDetails from "./pages/users/UserDetails";
 import PostDetails from "./pages/posts/PostDetails";
+import Login from "./pages/login/Login";
+import React from "react";
 
 export const BASE_URL = "https://jsonplaceholder.typicode.com";
 const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = React.useState(false);
   const myRoutes = [
     { path: "/", element: <Post /> },
     { path: "/users", element: <Users /> },
     { path: "/users/:id", element: <UserDetails /> },
     { path: "/post/:id", element: <PostDetails /> },
-    
   ];
-  const element = useRoutes(myRoutes);
+
+  const openRoutes = [{ path: "/login", element: <Login /> }];
+  
+  const element = useRoutes(isAuthenticated ? myRoutes : openRoutes);
   return <>{element}</>;
 };
-
 export default App;
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+import React from "react";
+
+const Login = () => {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const LOGIN_URL = "https://api.escuelajs.co/api/v1/auth/login";
+  const hitLogin = async () => {
+    const payload = {
+      email,
+      password: password,
+    };
+    const response = await axios.post(LOGIN_URL,payload);
+    localStorage.setItem("token", response.data.access_token);
+  };
+  return (
+    <div>
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={hitLogin} disabled={!email || !password}>
+        Login
+      </button>
+    </div>
+  );
+};
+
+export default Login;
